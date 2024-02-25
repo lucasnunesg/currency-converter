0f26a78557838cf4a6949e5c5b7fd255b079e4e4
Lucas Nunes <lucas.nunesgomes1@gmail.com>
2024-02-25 10:30:48 -0300

    added docstrings

--- app/infra/database.py
+++ app/infra/database.py
@@ -12,6 +12,20 @@ def mongodb_connect(
     host: str = "localhost",
     port: int = 27017,
 ) -> Collection:
+    """
+    Returns a pymongo collection connected to MongoDB
+
+            Parameters:
+                    database (str): Name of the database
+                    collection (str): Name of the collection
+                    host (str): Host address of the MongoDB server
+                    port (int): Port number
+
+
+            Returns:
+                    collection (pymongo.collection.Collection): MongoDB collection
+    """
+
     client = MongoClient(host=host, port=port)
 
     database = client[f"{database}"]
@@ -26,6 +40,15 @@ def update_conversion_collection(
     api: Type[CurrencyAPI] = EconomiaAwesomeAPI,
     currency_list: list = DEFAULT_CURRENCIES,
 ) -> None:
+    """
+    Creates a new document in the provided collection with updated currency conversion values based on the provided
+    API instance.
+
+            Parameters:
+                    db (pymongo.collection.Collection): pymongo collection to be updated (new document will be created)
+                    api (Type[CurrencyAPI]): API class (must be a valid CurrencyAPI subclass) to fetch data
+                    currency_list (list): List o currency names to be added to the database
+    """
     url = api.url_builder(currency_list)
     api.get_conversion(url, db)
 
