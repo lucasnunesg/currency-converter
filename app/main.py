5c676d4237ad06ee6e2adb8f6a7bd3b6e0a68649
Lucas Nunes <lucas.nunesgomes1@gmail.com>
2024-02-25 22:47:16 -0300

    added first test to root page

--- app/main.py
+++ app/main.py
@@ -2,9 +2,15 @@ from fastapi import FastAPI
 import uvicorn
 from app.api.v1 import routes
 
-app = FastAPI()
+app = FastAPI(title="Currency Conversion")
 
 app.include_router(routes.router)
+
+
+@app.get("/")
+def index():
+    return "Server is running!"
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="localhost", port=8081, reload=True)
