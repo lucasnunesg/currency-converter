41e7b5171e50f404b529019d8f028fa7fd10f6b9
Lucas Nunes <lucas.nunesgomes1@gmail.com>
2024-03-04 01:06:28 -0300

    added models

---
+++ app/api/v2/models.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from enum import Enum
+from typing import Optional
+
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class CurrencyType(Enum):
+
+    """Currency types: real or created by user."""
+    REAL = "real"
+    CUSTOM = "custom"
+    BACKING = "backing"
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Currency(Base):
+
+    """Currency model"""
+    __tablename__ = "currency"
+
+    id: Mapped[Optional[int]] = mapped_column(primary_key=True)
+    code: Mapped[str]
+    rate_usd: Mapped[float]
+    type: Mapped[CurrencyType]
+    update_time: Mapped[datetime]
